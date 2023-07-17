773e8fe03f958d16eb09a5af61026fa88669d366
Anwar3006 <anwarsadat.d2@gmail.com>
2023-07-17 20:30:33 +0000

    ecommerce-frontend

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -16,6 +16,7 @@ const Contact = () => {
         <div className="row">
           <div className="col-12">
             <iframe
+              title="map"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.9353459325403!2d-0.16379977524150102!3d5.576577044404015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9aba33111e9b%3A0x5205ac08e46586f5!2sLA%20Wireless%20Cluster%20of%20School%2C%20Labadi%20Link%2C%20Accra!5e0!3m2!1sen!2sgh!4v1689293020251!5m2!1sen!2sgh"
               width="600"
               height="450"
